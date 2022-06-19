5360ce32f7942b006de65c4f009de882724b78da
HungFai Koo <jhk888@yahoo.com>
2022-06-18 18:20:03 -0700

    update my state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello My friend!</h1>
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
