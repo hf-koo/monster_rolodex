9387aef3f99403c84c3f3245ceb52bed7d45a5d7
HungFai Koo <jhk888@yahoo.com>
2022-06-23 21:16:58 -0700

    refactor card component and add google fonts into index_html and finish class component app

--- src/card-list/card-list.component.jsx
+++ src/card-list/card-list.component.jsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
-
-import "../card-list/card.css";
+import Card from "../card/card.component";
+import "../card-list/card-list.css";
 
 export default class CardList extends Component {
   render() {
@@ -8,17 +8,7 @@ export default class CardList extends Component {
     return (
       <div className="card-list">
         {monsters.map((monster) => {
-          const { name, email, id } = monster;
-          return (
-            <div className="card-container">
-              <img
-                alt={`monster ${name}`}
-                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-              />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
-          );
+          return <Card monster={monster} />;
         })}
       </div>
     );
